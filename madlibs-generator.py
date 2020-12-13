a7b1f10f2a2e37c194ba4f41d415508f0de9be36
AmeyVijeesh <74102654+AmeyVijeesh@users.noreply.github.com>
2020-12-13 13:01:59 +0530

    Add more dialogues

---
+++ madlibs-generator.py
@@ -0,0 +1,6 @@
+name = input("Enter a name: ")
+verb = input("Enter a verb + ing: ")
+number = input("Enter a number: ")
+# You can add more
+output = f"Once upon a time, there lived a boy called {name}. He was {number} years old. He was {verb}."
+print(output)
