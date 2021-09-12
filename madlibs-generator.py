6353d6aae26cd9a3fc00c9f7e8562c829057f6d4
•AV• <74102654+AmeyVijeesh@users.noreply.github.com>
2021-09-12 18:43:40 +0530

    Update madlibs-generator.py

--- madlibs-generator.py
+++ madlibs-generator.py
@@ -3,4 +3,4 @@ verb = input("Enter a verb + ing: ")
 number = input("Enter a number: ")
 # You can add more
 output = f"Once upon a time, there lived a boy called {name}. He was {number} years old. He was {verb}."
-print(output)
+print(output) 
